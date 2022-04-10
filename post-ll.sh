bc1cb3c7a1dbd132ae33864530ba86e52051246d
Jonathan A. McCormick, Jr <67705789+McCormick9@users.noreply.github.com>
2022-04-10 13:41:44 -0400

    Update post-ll.sh

--- post-ll.sh
+++ post-ll.sh
@@ -1,1 +1,3 @@
 curl -X POST http://161.35.47.235:32475/api.php/city/ -d '{"city_name":"Liberpolis", "country_name":"Republic of Liberland"}' -H 'Content-Type: application/json'
+
+curl http://167.172.52.221:31546/api.php/city/liberpolis
